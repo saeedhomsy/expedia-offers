ca51ef0da964ffb55ebcd276d26f5c2da0a8c9fb
saeedhomsy <homsysaeed@gmail.com>
2017-04-01 21:44:00 +0300

    add jdk:

--- src/java/data.java
+++ src/java/data.java
@@ -10,13 +10,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import java.io.BufferedReader;
+
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.URL;
-import java.nio.charset.Charset;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.json.JSONArray;
